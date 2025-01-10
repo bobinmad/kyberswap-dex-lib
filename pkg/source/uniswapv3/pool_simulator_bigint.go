1885f2dca14b4200eda5f20fcbf59a19435189bc
Aleksandr Beleziak <abelezyak@irq.ru>
2025-01-10 17:05:58 +0300

    Fixes

--- pkg/source/uniswapv3/pool_simulator_bigint.go
+++ pkg/source/uniswapv3/pool_simulator_bigint.go
@@ -2,7 +2,6 @@ package uniswapv3
 
 import (
 	"errors"
-	"fmt"
 	"math/big"
 	"strings"
 
@@ -149,130 +148,6 @@ func (p *PoolSimulatorBigInt) getSqrtPriceLimit(zeroForOne bool) *big.Int {
 	}
 
 	return sqrtPriceX96Limit
-}
-
-func (p *PoolSimulatorBigInt) CalcAmountIn(param pool.CalcAmountInParams) (*pool.CalcAmountInResult, error) {
-	tokenAmountOut := param.TokenAmountOut
-	var tokenInIndex = p.GetTokenIndex(param.TokenIn)
-	var tokenOutIndex = p.GetTokenIndex(param.TokenAmountOut.Token)
-	var tokenOut *coreEntities.Token
-	var zeroForOne bool
-
-	if tokenInIndex >= 0 && tokenOutIndex >= 0 {
-		if strings.EqualFold(param.TokenAmountOut.Token, hexutil.Encode(p.V3Pool.Token0.Address[:])) {
-			zeroForOne = false
-			tokenOut = p.V3Pool.Token0
-		} else {
-			tokenOut = p.V3Pool.Token1
-			zeroForOne = true
-		}
-		amountOut := coreEntities.FromRawAmount(tokenOut, param.TokenAmountOut.Amount)
-		getInputAmountResult, err := p.V3Pool.GetInputAmount(amountOut, p.getSqrtPriceLimit(zeroForOne))
-
-		if err != nil {
-			return nil, fmt.Errorf("can not GetInputAmount, err: %+v", err)
-		}
-
-		amountIn := getInputAmountResult.ReturnedAmount
-		newPoolState := getInputAmountResult.NewPoolState
-
-		var remainingTokenAmountOut = &pool.TokenAmount{
-			Token: tokenAmountOut.Token,
-		}
-		if getInputAmountResult.RemainingAmountOut != nil {
-			remainingTokenAmountOut.Amount = getInputAmountResult.RemainingAmountOut.Quotient()
-		} else {
-			remainingTokenAmountOut.Amount = big.NewInt(0)
-		}
-
-		var totalGas = p.gas.BaseGas + p.gas.CrossInitTickGas*int64(getInputAmountResult.CrossInitTickLoops)
-
-		amountInBI := amountIn.Quotient()
-		if amountInBI.Cmp(zeroBI) > 0 {
-			return &pool.CalcAmountInResult{
-				TokenAmountIn: &pool.TokenAmount{
-					Token:  param.TokenIn,
-					Amount: amountInBI,
-				},
-				RemainingTokenAmountOut: remainingTokenAmountOut,
-				Fee: &pool.TokenAmount{
-					Token:  param.TokenIn,
-					Amount: nil,
-				},
-				Gas: totalGas,
-				SwapInfo: SwapInfoBigInt{
-					nextStateSqrtRatioX96: new(big.Int).Set(newPoolState.SqrtRatioX96),
-					nextStateLiquidity:    new(big.Int).Set(newPoolState.Liquidity),
-					nextStateTickCurrent:  newPoolState.TickCurrent,
-				},
-			}, nil
-		}
-
-		return nil, errors.New("amountIn is 0")
-	}
-
-	return nil, fmt.Errorf("tokenInIndex %v or tokenOutIndex %v is not correct", tokenInIndex, tokenOutIndex)
-}
-
-func (p *PoolSimulatorBigInt) CalcAmountOut(param pool.CalcAmountOutParams) (*pool.CalcAmountOutResult, error) {
-	tokenAmountIn := param.TokenAmountIn
-	tokenOut := param.TokenOut
-	var tokenInIndex = p.GetTokenIndex(tokenAmountIn.Token)
-	var tokenOutIndex = p.GetTokenIndex(tokenOut)
-	var tokenIn *coreEntities.Token
-	var zeroForOne bool
-
-	if tokenInIndex >= 0 && tokenOutIndex >= 0 {
-		if strings.EqualFold(tokenOut, hexutil.Encode(p.V3Pool.Token0.Address[:])) {
-			zeroForOne = false
-			tokenIn = p.V3Pool.Token1
-		} else {
-			tokenIn = p.V3Pool.Token0
-			zeroForOne = true
-		}
-		amountIn := coreEntities.FromRawAmount(tokenIn, tokenAmountIn.Amount)
-		amountOutResult, err := p.V3Pool.GetOutputAmount(amountIn, p.getSqrtPriceLimit(zeroForOne))
-
-		if err != nil {
-			return &pool.CalcAmountOutResult{}, fmt.Errorf("can not GetOutputAmount, err: %+v", err)
-		}
-
-		amountOut := amountOutResult.ReturnedAmount
-		newPoolState := amountOutResult.NewPoolState
-		var remainingTokenAmountIn = &pool.TokenAmount{
-			Token: tokenAmountIn.Token,
-		}
-		if amountOutResult.RemainingAmountIn != nil {
-			remainingTokenAmountIn.Amount = amountOutResult.RemainingAmountIn.Quotient()
-		} else {
-			remainingTokenAmountIn.Amount = big.NewInt(0)
-		}
-		var totalGas = p.gas.BaseGas + p.gas.CrossInitTickGas*int64(amountOutResult.CrossInitTickLoops)
-
-		if amountOut.Quotient().Cmp(zeroBI) > 0 {
-			return &pool.CalcAmountOutResult{
-				TokenAmountOut: &pool.TokenAmount{
-					Token:  tokenOut,
-					Amount: amountOut.Quotient(),
-				},
-				RemainingTokenAmountIn: remainingTokenAmountIn,
-				Fee: &pool.TokenAmount{
-					Token:  tokenAmountIn.Token,
-					Amount: nil,
-				},
-				Gas: totalGas,
-				SwapInfo: SwapInfoBigInt{
-					nextStateSqrtRatioX96: new(big.Int).Set(newPoolState.SqrtRatioX96),
-					nextStateLiquidity:    new(big.Int).Set(newPoolState.Liquidity),
-					nextStateTickCurrent:  newPoolState.TickCurrent,
-				},
-			}, nil
-		}
-
-		return &pool.CalcAmountOutResult{}, errors.New("amountOut is 0")
-	}
-
-	return &pool.CalcAmountOutResult{}, fmt.Errorf("tokenInIndex %v or tokenOutIndex %v is not correct", tokenInIndex, tokenOutIndex)
 }
 
 func (p *PoolSimulatorBigInt) UpdateBalance(params pool.UpdateBalanceParams) {
